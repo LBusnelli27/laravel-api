eb1ff0883521669faf97f745083b4b24c6c63d7f
Busna27 <lucabusnelli2001@gmail.com>
2022-05-26 17:44:36 +0200

    creates posts api

--- public/js/app.js
+++ public/js/app.js
@@ -53783,8 +53783,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/luca/Desktop/Esercizi Boolean/laravel-auth/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/luca/Desktop/Esercizi Boolean/laravel-auth/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/luca/Desktop/Esercizi Boolean/laravel-api/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/luca/Desktop/Esercizi Boolean/laravel-api/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/test', function(){
+    $posts = Post::all();
+
+    return response()->json($posts);
 });
