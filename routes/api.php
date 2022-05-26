f3512113da77f81a5a89f117785eea0702ea4c06
Busna27 <lucabusnelli2001@gmail.com>
2022-05-26 17:46:05 +0200

    rename api URI

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/test', function(){
+Route::get('/posts', function(){
     $posts = Post::all();
 
     return response()->json($posts);
